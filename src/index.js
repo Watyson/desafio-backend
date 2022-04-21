1479251f8ac11139e7990944910261e21dde2e27
Watyson <pacht662@gmail.com>
2022-04-21 18:24:33 -0300

    Update index.js
    Starting the server on PORT 3333 and the morgan and express modules.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express   = require("express")
+const morgan    = require("morgan")
+
+const PORT  = 3333
+const app   = express()
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}.`)
+})
