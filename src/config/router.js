ee03817a30602eccf6eaa5216bd33f84891c2133
Watyson <pacht662@gmail.com>
2022-04-25 01:10:56 -0300

    Routes changed due to bugs caused in the frontend

--- src/config/router.js
+++ src/config/router.js
@@ -4,7 +4,7 @@ const controllerSchedules = require("../controller/schedules")
 const router    = express.Router()
 
 router.post ("/api/toschedule", controllerSchedules.store)
-router.get  ("/api/schedules/:page%26:qtd", controllerSchedules.index)
-router.put  ("/api/schedules/:name%26:schedule", controllerSchedules.updateStatusTrue)
+router.get  ("/api/schedules/:page/:qtd", controllerSchedules.index)
+router.put  ("/api/schedules/:name/:schedule", controllerSchedules.updateStatusTrue)
 
 module.exports = router
