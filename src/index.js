1baa569493a39934c63a6e2c44d836e2f411177b
Watyson <pacht662@gmail.com>
2022-04-25 02:23:17 -0300

    CORS bug fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express   = require("express")
 const morgan    = require("morgan")
+const cors      = require("cors")
 const router    = require("./config/router")
 
 const PORT  = 3333
 const app   = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(router)
